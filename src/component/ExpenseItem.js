84d7f15101d0d39884fd78594b4ee9a5bf13a3f4
sri12345-del <seena.0196@gmail.com>
2023-04-25 11:06:16 +0530

    Expenseitem

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <p>Food rs 10</p>
+        <p>Petrol rs 100</p>
+        <p>Movies rs 1000</p>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
