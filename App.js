d5aa82f39fda7e18484e9f6bd05865fb88bb8bb4
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-05-16 13:12:39 +0530

    RestorentAppAddingSummary

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React,{Fragment} from "react";
 import Header from "./Components/Layout/Header";
+import Summary from "./Components/Meals/Summary";
 
 function App() {
   return (
     <Fragment>
-    
       <Header></Header>
+      <Summary></Summary>
     </Fragment>
   );
 }
