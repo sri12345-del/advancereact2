3181c22e6aa0626869e26e515922b05159b3496d
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-05 11:17:26 +0530

    forgetPasseordWork

--- Auth.js
+++ Auth.js
@@ -1,14 +1,19 @@
 import React, { useState, useRef, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import Autcontext from "../store/autcontext";
+import { Button, Card, Container, Form } from "react-bootstrap";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 const Login = () => {
   const history = useHistory();
   const [islogin, setislogin] = useState(false);
   const [formvalid, setformvalid] = useState(false);
   const [conformpassword, setconformpassword] = useState("");
+  const [resetpassword, setresetpassword] = useState(false);
   const emailval = useRef();
   const passwordval = useRef();
+
+  const resetemail = useRef();
 
   const autctx = useContext(Autcontext);
   const changestate = (e) => {
@@ -55,13 +60,13 @@ const Login = () => {
         }
       })
       .then((data) => {
-          console.log(data);
-          if (islogin) {
-              autctx.addtoken(data.idToken);
-              history.replace("/home");
-          } else {
-              setislogin(true)
-          }
+        console.log(data);
+        if (islogin) {
+          autctx.addtoken(data.idToken);
+          history.replace("/home");
+        } else {
+          setislogin(true);
+        }
       })
       .catch((err) => console.log(err.message));
 
@@ -69,29 +74,98 @@ const Login = () => {
     passwordval.current.value = "";
     setconformpassword("");
   };
+
+  const passwordreset = () => {
+    setresetpassword(true);
+  };
+
+  const resetPasswordHandler = (e) => {
+    e.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCenENgt4LrLH79u1_uh-2mToo1R_OEeRM",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "PASSWORD_RESET",
+          email: resetemail.current.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("somthing is wrong");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setresetpassword(false);
+      })
+      .catch((err) => console.log(err.message));
+  };
   return (
-    <form onSubmit={fetchhandler}>
-      {!islogin && <h2>Sign up</h2>}
-      {islogin && <h2>Login</h2>}
-      <label>Email</label>
-      <input type="text" id="email" ref={emailval}></input>
-      <label>Password</label>
-      <input type="number" id="password" ref={passwordval}></input>
-      {!islogin && <label>Conform Password</label>}
-      {!islogin && (
-        <input
-          type="number"
-          id="name"
-          onChange={conformhandler}
-          value={conformpassword}
-        ></input>
+    <Container>
+      {!resetpassword && (
+        <Form onSubmit={fetchhandler}>
+          {!islogin && <h2>Sign up</h2>}
+          {islogin && <h2>Login</h2>}
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="text" id="email" ref={emailval}></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="number"
+              id="password"
+              ref={passwordval}
+            ></Form.Control>
+          </Form.Group>
+          {!islogin && (
+            <Form.Group>
+              <Form.Label>Conform Password</Form.Label>
+              <Form.Control
+                type="number"
+                id="name"
+                onChange={conformhandler}
+                value={conformpassword}
+              ></Form.Control>
+            </Form.Group>
+          )}
+          <Button
+            style={{ backgroundColor: formvalid ? "green" : "lightblue" }}
+          >
+            {islogin ? "login" : "signup"}
+          </Button>
+          {islogin && <Button onClick={passwordreset}>Foget Password</Button>}
+          <Button onClick={changestate}>Have an account?login</Button>
+        </Form>
       )}
-      <button style={{ backgroundColor: formvalid ? "green" : "lightblue" }}>
-        {islogin ? "login" : "signup"}
-      </button>
-      {islogin && <a>Fogot password</a>}
-      <button onClick={changestate}>Have an account?login</button>
-    </form>
+      {resetpassword && (
+        <Card>
+          <Card.Title style={{ textAlign: "center" }}>
+            Reset password
+          </Card.Title>
+          <Form>
+            <Form.Group>
+              <Form.Label>Enter the email with which you regester</Form.Label>
+              <Form.Control
+                type="text"
+                id="email"
+                ref={resetemail}
+              ></Form.Control>
+            </Form.Group>
+            <Button variant="primary" onClick={resetPasswordHandler}>
+              Send link
+            </Button>
+          </Form>
+        </Card>
+      )}
+    </Container>
   );
 };
 
