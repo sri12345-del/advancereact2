6b88b0abeea7ae7f3eb916154b02479754012088
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-05 08:34:54 +0530

    LogoutUser

--- layout.js
+++ layout.js
@@ -1,9 +1,18 @@
 import Mainheader from "./Mainheader"
+import {useHistory} from "react-router-dom"
 
 const Layout = (props) => {
+
+    const history=useHistory()
+
+    const logouthandler = () => {
+        history.replace("/auth")
+        localStorage.removeItem("key")
+    }
     return (
         <div>
             <Mainheader></Mainheader>
+            {localStorage.getItem("key") && <div><button onClick={logouthandler}>Logout</button></div>}
             <main>{ props.children}</main>
         </div>
     )
