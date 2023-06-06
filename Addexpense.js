dda95317eefe2a7457ef18c25729f6cabd38ba75
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-06 11:56:29 +0530

    DeleteAndDelete

--- Addexpense.js
+++ Addexpense.js
@@ -1,82 +1,155 @@
-import React, { useEffect, useRef , useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Card, Container, Form } from "react-bootstrap";
 import Expenselist from "../Component/Expenselist";
 
 const AddExpense = () => {
+  const [money, setmoney] = useState("");
+  const [description, setdescription] = useState("");
+  const [catagory, setcatagory] = useState();
 
-  const money=useRef()
-  const description=useRef()
-  const catagory = useRef()
-  
-  const [expenseitem, setexpenseitem] = useState([])
-  
+  const moneyhandler = (e) => {
+    setmoney(e.target.value);
+  };
+  const descriptionhandler = (e) => {
+    setdescription(e.target.value);
+  };
+
+  const catagoryhandler = (e) => {
+    setcatagory(e.target.value);
+  };
+
+  const [expenseitem, setexpenseitem] = useState([]);
+
   useEffect(() => {
     fetch("https://react-http-735b2-default-rtdb.firebaseio.com/expense.json", {
       method: "GET",
-    }).then(res => {
-      if (!res.ok) {
-        throw new Error("smething is wrong")
-      } else {
-        return res.json()
-      }
-    }).then(data => {
-      for (let key in data) {
-        setexpenseitem(item=>[...item,data[key]])
-       }
-    }).catch(err=>console.log(err.message))
-  },[])
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("smething is wrong");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setexpenseitem([]);
+        for (let key in data) {
+          let newobj = { ...data[key], key: key };
+          setexpenseitem((item) => [...item, newobj]);
+        }
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
 
   const additemhandler = (e) => {
     e.preventDefault();
     let obj = {
-      money: money.current.value,
-      description: description.current.value,
-      catagory: catagory.current.value,
-      id:Math.random().toString()
-    }
-    console.log(obj)
+      money: money,
+      description: description,
+      catagory: catagory,
+      id: Math.random().toString(),
+    };
+
     fetch("https://react-http-735b2-default-rtdb.firebaseio.com/expense.json", {
       method: "POST",
       body: JSON.stringify(obj),
       headers: {
-        "Content-Type":"application/json"
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("something is wrong");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        let newobj = {
+          ...obj,
+          key: data.name,
+        };
+        setexpenseitem((item) => [...item, newobj]);
+      });
+  };
+
+  const deletehandler = (item) => {
+    setexpenseitem((data) => data.filter((val) => val.key != item));
+    fetch(
+      `https://react-http-735b2-default-rtdb.firebaseio.com/expense/${item}.json`,
+      {
+        method: "DELETE",
       }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("something is wrong");
+        } else {
+          return res.json();
+        }
+      })
+      .then(() => console.log("expense sucessfully deleted"))
+      .catch((err) => console.log(err));
+  };
+
+  const edithandler = (item) => {
+    setexpenseitem((data) => data.filter((val) => val.key != item));
+    const arr = expenseitem.filter(val=>val.key==item)
+    console.log(arr)
+    fetch(`https://react-http-735b2-default-rtdb.firebaseio.com/expense/${item}.json`, {
+      method: "PUT",
+      body: JSON.stringify({
+        money: money,
+        description: description,
+        catagory:catagory,
+      })
     }).then(res => {
       if (!res.ok) {
-        throw new Error("something is wrong")
+        throw new Error("somthing is wrong")
       } else {
         return res.json()
       }
-    }).then(data => {
-      console.log(data.name)
-    })
-    setexpenseitem(item=>[...item,obj])
-    
-  }
+    }).then(data => console.log(data))
+    .catch(err=>console.log(err))
+  };
   return (
-    <Container style={{alignItems:"center"}}>
-      <Card style={{width:"50%", padding:"2rem 3rem", margin:"5% 25%"}}>
+    <Container style={{ alignItems: "center" }}>
+      <Card style={{ width: "50%", padding: "2rem 3rem", margin: "5% 25%" }}>
         <Form>
           <Form.Group>
             <Form.Label>Money</Form.Label>
-            <Form.Control type="amount" ref={money}></Form.Control>
+            <Form.Control
+              type="amount"
+              onChange={moneyhandler}
+              value={money}
+            ></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" ref={description}></Form.Control>
+            <Form.Control
+              type="text"
+              onChange={descriptionhandler}
+              value={description}
+            ></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>Choose catagory</Form.Label>
-            <Form.Select ref={catagory}>
-              <option>food</option>
-              <option>petrol</option>
-              <option>salary</option>
+            <Form.Select onChange={catagoryhandler} value={catagory}>
+              <option value="food">food</option>
+              <option value="petrol">petrol</option>
+              <option value="salary">salary</option>
             </Form.Select>
           </Form.Group>
-          <Button variant="primary" onClick={additemhandler}>Additem</Button>
+          <Button variant="primary" onClick={additemhandler}>
+            Additem
+          </Button>
         </Form>
       </Card>
-      <Expenselist items={expenseitem}></Expenselist>
+      <Expenselist
+        items={expenseitem}
+        delete={deletehandler}
+        edit={edithandler}
+      ></Expenselist>
     </Container>
   );
 };
