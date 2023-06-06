5f81fd9cbbd5cd045e9c578bf0c401ff52dcb360
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-06 18:57:12 +0530

    IntegratingReactWithRedwx

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider} from "react-redux"
 
-import "./index.css";
-import App from "./App";
-import { Autcontextprovider } from "./store/autcontext";
+import './index.css';
+import App from './App';
+import store from './store/index';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Autcontextprovider>
-      <App />
-    </Autcontextprovider>
-  </BrowserRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Provider store={store}><App /></Provider>);
