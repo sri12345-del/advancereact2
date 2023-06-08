ffcbe9943e91d6526d33c9a1af67a506db1f3377
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-08 08:07:09 +0530

    addingPremiumFeature

--- Expenselist.js
+++ Expenselist.js
@@ -1,39 +1,56 @@
-import React from "react"
-import { Button, Card, Table } from "react-bootstrap"
+import React from "react";
+import { Button, Card, Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Expenselist = (props) => {
+    const listitem = useSelector((state) => state.expense.item);
+    console.log(listitem)
 
-    const deletehandler = (item) => {
-        props.delete(item)
-    }
+  const deletehandler = (item) => {
+    props.delete(item);
+  };
 
-    const edithandler = (item) => {
-        props.edit(item)
-    }
-    return (
-        <Card style={{margin:"0rem 25%", padding:"1rem 3rem"}}>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>Money</th>
-                        <th>Description</th>
-                        <th>Catagory</th>
-                        <th>Delete</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody>{props.items.map(item => (
-                    <tr key={ item.id} id={item.id}>
-                        <td>{ item.money}</td>
-                        <td>{ item.description}</td>
-                        <td>{item.catagory}</td>
-                        <td><Button onClick={deletehandler.bind(null,item.key)}>Delete</Button></td>
-                        <td><Button onClick={edithandler.bind(null,item.key)}>Edit</Button></td>
-                    </tr>
-                )) }</tbody>
-           </Table>
-        </Card>
-    )
-}
+  const edithandler = (item) => {
+    props.edit(item);
+  };
 
-export default Expenselist
+  const blob = new Blob([JSON.stringify(listitem)]);
+  const herf = URL.createObjectURL(blob);
+  return (
+    <Card style={{ margin: "0rem 25%", padding: "1rem 3rem" }}>
+      <Table>
+        <thead>
+          <tr>
+            <th>Money</th>
+            <th>Description</th>
+            <th>Catagory</th>
+            <th>Delete</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.items.map((item) => (
+            <tr key={item.id} id={item.id}>
+              <td>{item.money}</td>
+              <td>{item.description}</td>
+              <td>{item.catagory}</td>
+              <td>
+                <Button onClick={deletehandler.bind(null, item.key)}>
+                  Delete
+                </Button>
+              </td>
+              <td>
+                <Button onClick={edithandler.bind(null, item.key)}>Edit</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <a href={herf} download="file.csv">
+        download
+      </a>
+    </Card>
+  );
+};
+
+export default Expenselist;
