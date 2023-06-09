cdb84f32ef4c5b09628f44a4918bb71aaa1982bb
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-09 07:47:48 +0530

    FixingCartProblem

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { notfyaction } from "./store/notifyslice";
+import { cartitemaction } from "./store/cartitem";
+
+let inital=true
 
 function App() {
   const cartvisible = useSelector((state) => state.cart.isopen);
@@ -13,24 +16,55 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(notfyaction.loodingstate());
+    function sendcartdata(){
+      dispatch(notfyaction.loodingstate());
+      if (inital) {
+        inital = false
+        return
+      }
+      fetch(
+        "https://react-http-735b2-default-rtdb.firebaseio.com/cartitem.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart.item),
+        }
+      )
+        .then((res) => {
+          if (res.ok) {
+            dispatch(notfyaction.loodingsucess());
+          }
+        })
+        .catch((err) => {
+          dispatch(notfyaction.loodingerror());
+        });
+    }
+    if (inital) {
+      inital = false
+      return
+    }
+    sendcartdata()
+  }, [cart]);
+
+  useEffect(() => {
     fetch(
       "https://react-http-735b2-default-rtdb.firebaseio.com/cartitem.json",
       {
-        method: "PUT",
-        body: JSON.stringify(cart),
+        method: "GET",
       }
     )
       .then((res) => {
-        console.log("sucess")
-        if (res.ok) {
-          dispatch(notfyaction.loodingsucess());
+        if (!res.ok) {
+          throw new Error("somthing is wrong");
+        } else {
+          return res.json();
         }
       })
-      .catch((err) => {
-        dispatch(notfyaction.loodingerror());
-      });
-  }, [cart]);
+      .then((data) => {
+        console.log("sucess")
+        data.map((item) => dispatch(cartitemaction.addcartitem(item)));
+      })
+      .catch((err) => err);
+  }, []);
   return (
     <Layout>
       {cartvisible && <Cart />}
