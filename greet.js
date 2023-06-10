87ffe86ed12cddc35a7017c0960424d0266acf13
sri12345-del <129243474+sri12345-del@users.noreply.github.com>
2023-06-10 12:47:23 +0530

    testingUserAction

--- greet.js
+++ greet.js
@@ -1,8 +1,19 @@
+import React,{useState} from "react"
+
 const Greet = () => {
+    const [isshow, setisshow]=useState(false)
+
+    const changehandler = () => {
+        setisshow(true)
+    }
+
     return (
         <div>
             <h1>hello world</h1>
-            <h1>price</h1>
+            <p>price</p>
+            {isshow && <p>it is visible</p>}
+            {!isshow && <p>it is not visible</p>} 
+            <button onClick={changehandler}>change content</button>
         </div>
     )
 }
--- greet.test.js
+++ greet.test.js
@@ -1,4 +1,5 @@
-import { render ,screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greet from "./greet";
 
 describe("alltest", () => {
@@ -6,7 +7,36 @@ describe("alltest", () => {
         render(<Greet></Greet>)
 
         const money = screen.getByText("hello world")
-        expect(money).toBeInTheDocument()      
+        expect(money).toBeInTheDocument();
+    });
+
+    test("expenselist2", () => {
+        render(<Greet></Greet>)
+        const price = screen.getByText("price")
+        expect(price).toBeInTheDocument();
+    });
+    test("test3", () => {
+        render(<Greet></Greet>)
+
+        const isvisible = screen.getByText("it is not visible")
+        expect(isvisible).toBeInTheDocument()
+    })
+    test("test4", () => {
+        render(<Greet></Greet>)
+        
+        const button = screen.getByRole("button")
+        userEvent.click(button)
+
+        const visible = screen.getByText("it is visible")
+        expect(visible).toBeInTheDocument()
+    })
+    test("does not render button is clicked", () => {
+        render(<Greet></Greet>)
+        const button = screen.getByRole("button")
+        userEvent.click(button)
+
+        const outputelement = screen.queryByText("it is not visible")
+        expect(outputelement).toBeNull()
     })
 })
         
